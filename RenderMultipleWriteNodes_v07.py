8f94918bfa408086419e39126e52099195ab7b37
tharunganapathi <65713157+tharunganapathi@users.noreply.github.com>
2021-02-19 19:46:08 +0530

    Update RenderMultipleWriteNodes_v07.py

--- RenderMultipleWriteNodes_v07.py
+++ RenderMultipleWriteNodes_v07.py
@@ -26,31 +26,3 @@ def RenderMultipleNodes():
             render_information.append(nodes)
             nuke.display('showChannels()', node, 'Render_Information')
 
-
-
-
-
-
-def select_All_Writenodes():
-    for i in nuke.allNodes('Write'):
-        i.setSelected(True)    
-
-
-def setFrameRange():
-    for i in nuke.allNodes('Write'):
-        frameRange = i.frameRange()
-        firstFrame = frameRange.first()
-        lastFrame = frameRange.last()
-        print i.name()
-        print firstFrame
-        print lastFrame
-        i.knob('use_limit').setValue(True)
-        i.knob('first').setValue(firstFrame)
-        i.knob('last').setValue(lastFrame)
-        print '\n'
-
-  
-
-nuke.menu('Nodes').addMenu('RenderMultipleNodes',icon = 'Render.png').addCommand('Select all Write nodes', select_All_Writenodes,icon = 'Write.png')
-nuke.menu('Nodes').addMenu('RenderMultipleNodes',icon = 'Render.png').addCommand('Set Frame Range', setFrameRange, icon = 'FrameRange.png')
-nuke.menu('Nodes').addMenu('RenderMultipleNodes',icon = 'Render.png').addCommand('Render selected Write nodes', RenderMultipleNodes, icon = 'ProjectionSolver.png')
